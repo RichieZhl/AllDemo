68504e1fd905acd03da5ca5a98c0bf30d87eb2c9
RichieZhl <lylaut@163.com>
2019-05-24 17:13:28 +0800

    config-server

--- eureka-server/src/main/java/com/richie/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/richie/eurekaserver/EurekaServerApplication.java
@@ -2,11 +2,9 @@ package com.richie.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
-@EnableConfigServer
 @SpringBootApplication
 public class EurekaServerApplication {
 
